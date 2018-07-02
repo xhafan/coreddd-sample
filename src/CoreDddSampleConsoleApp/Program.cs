58d4df480ba46db34603fe3c33aa70c6d3da6d48
xhafan <xhafan@gmail.com>
2018-07-02 23:03:48 +0200

    added .net core console app

---
+++ src/CoreDddSampleConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreDddSampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
