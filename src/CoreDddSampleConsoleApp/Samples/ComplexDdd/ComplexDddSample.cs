cbe274e35fb217e0d222f3bff319d5811a019e99
xhafan <xhafan@gmail.com>
2018-07-25 09:35:39 +0200

    refactored DDD sample to make it more readable

--- src/CoreDddSampleConsoleApp/Samples/ComplexDdd/ComplexDddSample.cs
+++ src/CoreDddSampleConsoleApp/Samples/ComplexDdd/ComplexDddSample.cs
@@ -35,36 +35,8 @@ namespace CoreDddSampleConsoleApp.Samples.ComplexDdd
                     var policyHolderService = ioCContainer.Resolve<PolicyHolderService>();
                     var policyService = ioCContainer.Resolve<PolicyService>();
 
-                    var shipOneId = await shipService.CreateNewShipAsync("lady star", 10m);
-                    var shipTwoId = await shipService.CreateNewShipAsync("golden sea", 20m);
-                    var newPolicyHolderId = await policyHolderService.CreateNewPolicyHolderAsync("Policy holder name");
-
-                    var today = DateTime.Today;
-                    var todayPlusOneYear = today.AddYears(1);
-
-                    var policyOneId = await policyService.CreateNewPolicyAsync(
-                        policyHolderId: newPolicyHolderId,
-                        startDate: today,
-                        endDate: todayPlusOneYear,
-                        terms: "policy one terms"
-                    );
-                    await policyService.AddShipToPolicyAsync(policyOneId, shipOneId, insuredTonnage: 8m, ratePerTonnage: 5m);
-                    await policyService.AddShipToPolicyAsync(policyOneId, shipTwoId, insuredTonnage: 18m, ratePerTonnage: 6m);
-
-                    var policyTwoId = await policyService.CreateNewPolicyAsync(
-                        policyHolderId: newPolicyHolderId,
-                        startDate: today,
-                        endDate: todayPlusOneYear,
-                        terms: "policy two terms"
-                    );
-
-                    unitOfWork.Flush();
-
-                    var (policyDtos, shipCargoPolicyItemDtos) = await policyService.GetResultFromMultipleQueries(policyTerms: "one", shipName: "golden");
-
-                    Console.WriteLine($"Policies by terms query was executed. Number of policy dtos queried: {policyDtos.Count()}");
-                    Console.WriteLine($"Ship cargo policy items by ship name query was executed. Number of ship cargo policy item dtos queried: " +
-                                      $"{shipCargoPolicyItemDtos.Count()}");
+                    await _CreateEntitiesUsingCommands(shipService, policyHolderService, policyService, unitOfWork);
+                    await _QueryOverCreatedEntities(policyService);
 
                     await unitOfWork.CommitAsync();
                 }
@@ -80,6 +52,50 @@ namespace CoreDddSampleConsoleApp.Samples.ComplexDdd
             }
 
             ioCContainer.Dispose();
+        }
+
+        private async Task _CreateEntitiesUsingCommands(
+            ShipService shipService, 
+            PolicyHolderService policyHolderService,
+            PolicyService policyService, 
+            NhibernateUnitOfWork nhibernateUnitOfWork
+            )
+        {
+            var shipOneId = await shipService.CreateNewShipAsync("lady star", 10m);
+            var shipTwoId = await shipService.CreateNewShipAsync("golden sea", 20m);
+            var newPolicyHolderId = await policyHolderService.CreateNewPolicyHolderAsync("Policy holder name");
+
+            var today = DateTime.Today;
+            var todayPlusOneYear = today.AddYears(1);
+
+            var policyOneId = await policyService.CreateNewPolicyAsync(
+                policyHolderId: newPolicyHolderId,
+                startDate: today,
+                endDate: todayPlusOneYear,
+                terms: "policy one terms"
+            );
+            await policyService.AddShipToPolicyAsync(policyOneId, shipOneId, insuredTonnage: 8m, ratePerTonnage: 5m);
+            await policyService.AddShipToPolicyAsync(policyOneId, shipTwoId, insuredTonnage: 18m, ratePerTonnage: 6m);
+
+            var policyTwoId = await policyService.CreateNewPolicyAsync(
+                policyHolderId: newPolicyHolderId,
+                startDate: today,
+                endDate: todayPlusOneYear,
+                terms: "policy two terms"
+            );
+
+            nhibernateUnitOfWork.Flush();
+        }
+
+        private async Task _QueryOverCreatedEntities(PolicyService policyService)
+        {
+            var (policyDtos, shipCargoPolicyItemDtos) =
+                await policyService.GetResultFromMultipleQueries(policyTerms: "one", shipName: "golden");
+
+            Console.WriteLine($"Policies by terms query was executed. Number of policy dtos queried: {policyDtos.Count()}");
+            Console.WriteLine(
+                $"Ship cargo policy items by ship name query was executed. Number of ship cargo policy item dtos queried: " +
+                $"{shipCargoPolicyItemDtos.Count()}");
         }
 
         private void _RegisterComponents(WindsorContainer ioCContainer)
