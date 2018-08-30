20c8169c1605c7de1b5cf44a0cb46bb74a792575
xhafan <xhafan@gmail.com>
2018-08-30 15:51:11 +0200

    added default ASP.NET Core MVC generated project (no modification from the default project template)

---
+++ src/CoreDddAspNetCoreWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDddAspNetCoreWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
