2d7f53c556e4409844b427f447dc41e962d80810
xhafan <xhafan@gmail.com>
2018-09-07 15:40:41 +0200

    Sample ASP.NET web app - handling each request in an ADO.NET transaction via UnitOfWorkHttpModule

---
+++ src/CoreDddSampleAspNetWebApp/CoreDddSampleNhibernateConfigurator.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using CoreDdd.Nhibernate.Configurations;
+
+namespace CoreDddSampleAspNetWebApp
+{
+    public class CoreDddSampleNhibernateConfigurator : NhibernateConfigurator
+    {
+        protected override Assembly[] GetAssembliesToMap()
+        {
+            return new Assembly[0];
+        }
+    }
+}
--- src/CoreDddSampleAspNetWebApp/Global.asax.cs
+++ src/CoreDddSampleAspNetWebApp/Global.asax.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using CoreDdd.AspNet.HttpModules;
+using CoreDdd.Domain.Events;
+using CoreDdd.Nhibernate.Configurations;
+using CoreDdd.Nhibernate.Register.Castle;
+using CoreDdd.Register.Castle;
+using CoreDdd.UnitOfWorks;
 
 namespace CoreDddSampleAspNetWebApp
 {
@@ -16,6 +26,24 @@ namespace CoreDddSampleAspNetWebApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            var iocContainer = new WindsorContainer();
+
+            CoreDddNhibernateInstaller.SetUnitOfWorkLifeStyle(x => x.PerWebRequest);
+
+            iocContainer.Install(
+                FromAssembly.Containing<CoreDddInstaller>(),
+                FromAssembly.Containing<CoreDddNhibernateInstaller>()
+            );
+            iocContainer.Register(
+                Component
+                    .For<INhibernateConfigurator>()
+                    .ImplementedBy<CoreDddSampleNhibernateConfigurator>()
+                    .LifestyleSingleton()
+            );
+
+            UnitOfWorkHttpModule.Initialize(iocContainer.Resolve<IUnitOfWorkFactory>());
         }
     }
 }
